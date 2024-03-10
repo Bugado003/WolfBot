5c95d8f86a4baa589caefd09b9a04e0d35a6093b
Bugado003 <126205453+Bugado003@users.noreply.github.com>
2024-03-10 13:53:56 -0300

    Add files via upload

---
+++ JSONs/JsonReader.cs
@@ -0,0 +1,42 @@
+﻿//Pretty self-explanatory, right?
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Json.Serialization.Metadata;
+namespace LunariumWolf.JSONs
+{
+    internal class JsonReader
+    {
+        //Data that need to be extracted
+        public string token { get; set; }
+
+        public async Task Read()
+        {
+            //Use StreamReader to Read the Json
+           using (StreamReader sr = new StreamReader("config.json"))
+            {
+                //raw json
+                string json = await sr.ReadToEndAsync();
+                //JsonSerializerOptions
+                var options = new JsonSerializerOptions
+                {
+                    TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+                };
+
+                //Extracted data
+                JsonStructure data = JsonSerializer.Deserialize<JsonStructure>(json, options);
+                //Set values
+                this.token = data.token;
+            }
+        }
+    }
+    internal sealed class JsonStructure
+    {
+        //What is present in the Json
+        public string token { get; set; }
+    }
+}
---
+++ Source/Commands/BasicSlash.cs
@@ -0,0 +1,19 @@
+﻿//Basically a "list" with all the slash commands
+using DSharpPlus.SlashCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunariumWolf.Source.Commands
+{
+    internal class BasicSlash : ApplicationCommandModule
+    {
+        [SlashCommand("Register/ReacionRole", "Yea u could just use carl bot but are using this")]
+        public async Task Register(InteractionContext ctx)
+        {
+
+        }
+    }
+}
