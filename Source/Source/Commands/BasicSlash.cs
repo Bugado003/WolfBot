2ff8558f344fa092559c31f698a011f1c193b304
Bugado003 <126205453+Bugado003@users.noreply.github.com>
2024-03-12 16:00:36 -0300

    Add files via upload

---
+++ Source/Source/Commands/BasicSlash.cs
@@ -0,0 +1,30 @@
+﻿//Basically a "list" with all the basic slash commands
+using DSharpPlus.SlashCommands;
+using LunariumWolf.JSONs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunariumWolf.Source.Commands
+{
+    internal class BasicSlash : ApplicationCommandModule
+    {
+        //Download command
+        [SlashCommand("Get Download", "Someone's pc has to execute its commands")]
+        public async Task Download(InteractionContext ctx)
+        {
+            var jsonReader = new JsonReader();
+            jsonReader.Read();
+            //Try to send the file
+            try
+            {
+                ctx.Channel.SendMessageAsync(jsonReader.download);
+            } catch(Exception E) //If not able to
+            {
+                ctx.Channel.SendMessageAsync("Ops, something went wrong");
+            }
+        }
+    }
+}
